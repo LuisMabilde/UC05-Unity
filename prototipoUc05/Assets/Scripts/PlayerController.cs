c8cbb50aed133f289ae3352becd7f9db4e85e13a
LuisMabilde <45721122+LuisMabilde@users.noreply.github.com>
2019-11-04 22:06:45 -0300

    Projeto e codigo de player

---
+++ prototipoUc05/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	public Transform posPe;
+	[HideInInspector] public bool tocaChao = false;
+
+
+	public float Velocidade;
+	public float ForcaPulo = 1000f;
+	[HideInInspector] public bool viradoDireita = true;
+
+	public Image vida;
+	private MensagemControle MC;
+
+    public Transform bulletspawn;
+    public GameObject bulletObject;
+    public float fireRate;
+    public float nextFire;
+
+    void Start()
+    {
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		GameObject mensagemControleObject = GameObject.FindWithTag ("MensagemControle");
+		if (mensagemControleObject != null) {
+			MC = mensagemControleObject.GetComponent<MensagemControle> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Implementar Pulo Aqui! 
+	}
+
+	void FixedUpdate()
+	{
+		float translationY = 0;
+		float translationX = Input.GetAxis ("Horizontal") * Velocidade;
+		transform.Translate (translationX, translationY, 0);
+		transform.Rotate (0, 0, 0);
+		if (translationX != 0) {
+			anim.SetTrigger ("corre");
+		} else {
+			anim.SetTrigger("parado");
+		}
+
+        if (Input.GetButton("Fire1"))
+        {
+            Fire();
+        }
+		//Programar o pulo Aqui! 
+
+		if (translationX > 0 && !viradoDireita) {
+			Flip ();
+		} else if (translationX < 0 && viradoDireita) {
+			Flip();
+		}
+
+	}
+	void Flip()
+	{
+		viradoDireita = !viradoDireita;
+		Vector3 escala = transform.localScale;
+		escala.x *= -1;
+		transform.localScale = escala;
+	}
+
+	public void SubtraiVida()
+	{
+		vida.fillAmount-=0.1f;
+		if (vida.fillAmount <= 0) {
+			MC.GameOver();
+			Destroy(gameObject);
+		}
+	}
+	
+    void Fire()
+    {
+        anim.SetTrigger("Fire");
+        nextFire = Time.time + fireRate;
+
+        GameObject cloneBullet = Instantiate(bulletObject, bulletspawn.position, bulletspawn.rotation);
+
+    
+        
+    }
+
+}
---
+++ prototipoUc05/Assets/Scripts/arma1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class arma1 : MonoBehaviour
+{
+    public int arma1equipada = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            arma1equipada = arma1equipada++;
+        }
+       
+    }
+}
